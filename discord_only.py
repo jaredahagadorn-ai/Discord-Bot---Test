851b5faa61d4b31b569bcab6cd8da8cd90e5ec2a
jaredahagadorn-ai <jaredahagadorn@gmail.com>
2026-04-10 15:57:58 -0400

    Fix indentation and formatting issues in discord_only.py

--- discord_only.py
+++ discord_only.py
@@ -7,20 +7,20 @@ load_dotenv()
 
 # Set up intents
 intents = discord.Intents.default()
-intents.message_content = True # Ensure that your bot can read message content
+intents.message_content = True  # Ensure that your bot can read message content
 
 client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-  print ('We have logged in as {0.user}'.format(client)
+    print('We have logged in as {0.user}'.format(client))
 
 @client.event
 async def on_message(message):
-  if message.author == client.user:
-    return
-    
-  if message.content.startswith('$hello'):
-    await message.channel.send('Hello!')
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
 
 client.run(os.getenv('TOKEN'))
